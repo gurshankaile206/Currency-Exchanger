31cf06d14662c872fa3949723c767d1672b1121b
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-11 21:59:36 -0700

    change variable names of amount-one and amount-two from p1/p2 to a1/a2

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import CurrencyService  from './Currency';
 const call = async() => {
   let c1 = document.getElementById('currency-one').value; 
   let c2 = document.getElementById('currency-two').value;
-  let p1 = document.getElementById('amount-one').value;
-  let p2 = document.getElementById('amount-two').value;
+  let a1 = document.getElementById('amount-one').value;
+  let a2 = document.getElementById('amount-two').value;
   let api = await CurrencyService.getCurrency(c1);
 
   let value = api.conversion_rates[c2]
+  
 
 }
