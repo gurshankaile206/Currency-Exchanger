44aae68278d1c78a8ce1f9f9115998ad04ffd581
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-11 22:12:13 -0700

    add logic to change amount-two value to exchange rate amount

--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ import CurrencyService  from './Currency';
 
 const call = async() => {
   let c1 = document.getElementById('currency-one').value; 
-  let c2 = document.getElementById('currency-two').value;
+  let c2 = document.getElementById('currency-two').value; 
   let a1 = document.getElementById('amount-one').value;
   let a2 = document.getElementById('amount-two').value;
   let api = await CurrencyService.getCurrency(c1);
 
-  let value = api.conversion_rates[c2]
+  let value = api.conversion_rates[c2] 
   
+  a1 = parseInt(a1)  
+  if(!Number.isNaN(a1)) {
+    document.getElementById('amount-two').value = a1 * value 
+  }
 
 }
